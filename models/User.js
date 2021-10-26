bd24316af9625e3130158effea80d5c4b5e40ce6
Carlos Martinez <curlosmart@gmail.com>
2021-10-26 00:01:53 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true},
+  username: { type: String },
+  email: { type: String, required: true},
+  password: { type: String, required: true},
+  profilePic: { type: String },
+  bio: { type: String },
+  tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
+  retweets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' }],
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  lowerCaseEmail: { type: String, lowercase: true, trim: true, required: true },
+  },
+{ timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User;
